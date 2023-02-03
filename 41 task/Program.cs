4cc6020e302f72aa0446f2984eb741c40e4a7d18
Mansur Dagaev <mans.4e4en@mail.ru>
2023-02-03 17:05:05 +0300

    1

---
+++ 41 task/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+class Program
+{
+static void Main()
+
+{
+
+
+Console.Write("Введите элементы(через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов > 0: {count}");
+
+}
+}
+}
