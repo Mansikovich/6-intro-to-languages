7b341e650cb9c98e13b3c7c007820a81e2ee8696
Mansur Dagaev <mans.4e4en@mail.ru>
2023-02-03 17:15:31 +0300

    2

---
+++ 43 Task/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+class Program
+{
+static void Main()
+
+{
+
+Console.WriteLine("введите значение b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (b1 - b2)/(k2 - k1);
+double y = k2 * x + b2;
+
+Console.WriteLine($"({x}, {y})");
+
+
+
+}
+}
+}
